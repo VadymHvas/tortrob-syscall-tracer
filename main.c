866c330667fb3c998591ec0422d545b847811040
Vadym Hvas <hvassrim@gmail.com>
2026-04-12 14:16:36 +0300

    main: change type from pid_t* to pid_t

--- main.c
+++ main.c
@@ -14,18 +14,18 @@ int main(int argc, char **argv)
         }
 
         struct trace_opts *opts = parse_opts(argc, argv);
-        pid_t *child;
+        pid_t child;
 
         if (!opts)
                 return 1;
 
-        if (init_trace(argv, opts, child) == -1)
+        if (init_trace(argv, opts, &child) == -1)
                 goto err;
 
-        trace_loop(*child);
+        trace_loop(child);
 
         putc('\n', stdout);
-        cleanup_trace(opts, *child);
+        cleanup_trace(opts, child);
         return 0;
 err:
         free(opts);
